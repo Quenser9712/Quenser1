ab56be3774dca6e5416a6e7b4cccfc22bf28e9f4
Tassana Khrueawan <Quenser9712@hotmail.com>
2022-07-19 15:27:36 +0700

    'test';

--- src/App.vue
+++ src/App.vue
@@ -1,83 +1,19 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-import test from './components/test.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-    <test></test>
-  </main>
+  <div class="body">
+    <h1>APP</h1>
+    <test />
+  </div>
 </template>
 
-<style>
-@import './assets/base.css';
+<script>
+import test from './components/test.vue'
 
-#app {
-  max-width: 1280px;
-  margin: 0 auto;
-  padding: 2rem;
-
-  font-weight: normal;
+export default {
+  components: {
+    test,
+ 
+  },
 }
+</script>
 
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-a,
-.green {
-  text-decoration: none;
-  color: hsla(160, 100%, 37%, 1);
-  transition: 0.4s;
-}
-
-@media (hover: hover) {
-  a:hover {
-    background-color: hsla(160, 100%, 37%, 0.2);
-  }
-}
-
-@media (min-width: 1024px) {
-  body {
-    display: flex;
-    place-items: center;
-  }
-
-  #app {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    padding: 0 2rem;
-  }
-
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-}
-</style>
+<style></style>
--- src/components/test.vue
+++ src/components/test.vue
@@ -1,15 +1,48 @@
 <template>
   <div>
-    <h1>1111</h1>
+    <div class="watch">
+      <div class="time">00:00:00</div>
+      <div class="btn">
+        <button id="start" @click="time()">start</button>
+        <button id="stop" @click="stop()">stop</button>
+        <button id="reset" @click="reset()">reset</button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
 
+export default {
+  mounted() {},
+  
+  methods: {
+    time() {
+      const time = document.querySelector('.time')
+      let sss = 0
+      let mmm = 59
+      let hhh = 23
+
+     setInterval(() => {
+        sss++
+        if (sss == 60) {
+          mmm++
+          if (mmm == 60) {
+            hhh++
+            hhh = hhh % 24
+          }
+          mmm = mmm % 60
+        }
+        sss = sss % 60
+        time.innerText = hhh + ':' + mmm + ':' + sss
+      }, 1000)
+   
+    },
+    reset() {
+      const time = document.querySelector('.time')
+      time.innerText = '00:00:00'
+    },
+  },
 }
 </script>
-
-<style>
-
-</style>
+<style scoped></style>
