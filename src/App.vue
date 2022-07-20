e5253324f7c6375995fc5bca0a09adc5497a5ca4
Tassana Khrueawan <Quenser9712@hotmail.com>
2022-07-20 14:13:49 +0700

    'test';

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="body">
     <h1>APP</h1>
-    <test />
+    <test
+      :timer="formattedTime"
+      :state="timerState"
+      @start="start"
+      @pause="pause"
+      @stop="stop"
+    />
   </div>
 </template>
 
@@ -11,7 +17,45 @@ import test from './components/test.vue'
 export default {
   components: {
     test,
- 
+  },
+  data() {
+    return {
+      timerState: 'stopped',
+      currentTimer: 0,
+      formattedTime: '00:00:00',
+      ticker: undefined,
+      snackbar: false,
+    }
+  },
+  methods: {
+    start() {
+      if (this.timerState !== 'running') {
+        this.tick()
+        this.timerState = 'running'
+      }
+    },
+    pause() {
+      window.clearInterval(this.ticker)
+      this.timerState = 'paused'
+    },
+    stop() {
+      window.clearInterval(this.ticker)
+      this.currentTimer = 0
+      this.formattedTime = '00:00:00'
+      this.timerState = 'stopped'
+    },
+    tick() {
+      this.ticker = setInterval(() => {
+        this.currentTimer++
+        this.formattedTime = this.formatTime(this.currentTimer)
+      }, 250)
+    },
+    formatTime(seconds) {
+      let measuredTime = new Date(null)
+      measuredTime.setSeconds(seconds)
+      let MHSTime = measuredTime.toISOString().substr(11, 8)
+      return MHSTime
+    },
   },
 }
 </script>
--- src/components/test.vue
+++ src/components/test.vue
@@ -1,48 +1,29 @@
 <template>
   <div>
     <div class="watch">
-      <div class="time">00:00:00</div>
-      <div class="btn">
-        <button id="start" @click="time()">start</button>
-        <button id="stop" @click="stop()">stop</button>
-        <button id="reset" @click="reset()">reset</button>
+      <div>
+        {{ timer }}
       </div>
+      <button @click="$emit('start')" :disabled="state == 'running'">
+        Start
+      </button>
+      <button
+        @click="$emit('pause')"
+        :disabled="state == 'stopped' || state == 'paused'"
+      >
+        Pause
+      </button>
+      <button @click="$emit('stop')" :disabled="state == 'stopped'">
+        Stop
+      </button>
     </div>
   </div>
-</template>
+</template> 
 
 <script>
-
 export default {
-  mounted() {},
-  
-  methods: {
-    time() {
-      const time = document.querySelector('.time')
-      let sss = 0
-      let mmm = 59
-      let hhh = 23
-
-     setInterval(() => {
-        sss++
-        if (sss == 60) {
-          mmm++
-          if (mmm == 60) {
-            hhh++
-            hhh = hhh % 24
-          }
-          mmm = mmm % 60
-        }
-        sss = sss % 60
-        time.innerText = hhh + ':' + mmm + ':' + sss
-      }, 1000)
-   
-    },
-    reset() {
-      const time = document.querySelector('.time')
-      time.innerText = '00:00:00'
-    },
-  },
+  name: 'Timer',
+  props: ['timer', 'state'],
 }
 </script>
 <style scoped></style>
